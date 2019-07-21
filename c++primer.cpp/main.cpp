bf2578ebbce12df379c6dff0c0a11585b96a52b5
zhaogangang <44504986+zhaogangang@users.noreply.github.com>
2019-07-21 17:58:05 +0800

    new commit

--- c++primer.cpp/main.cpp
+++ c++primer.cpp/main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <string>
 #include <array>
 #include <vector>
 
 using std::cout;
 using std::endl;
 
+class test {
+private:
+	int i;
+public:
+	int get() {
+		return i;
+	}
+};
+
+void go(int num) {
+	cout << "go num" << endl;
+}
+
 
 int main(int argc, char** argv)
 {
-	cout << sizeof(bool) << endl;
+	int *p = NULL;
+	go(p);
 	system("pause");
 	return 0;
 }
+int i = 1;
